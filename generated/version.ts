0cdc5be7e969032a920bb68a9b6e77bcc6d38e8d
deakos808 <markdeakos@gmail.com>
2025-09-22 20:33:20 -1000

    checkpoint 20250922-203320 before signout+role fixes

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-09-23T06:18:06.418Z';
-export const GIT_HASH = 'c4015ee';
+export const DEPLOYED_AT = '2025-09-23T06:33:09.212Z';
+export const GIT_HASH = 'd36096d';
