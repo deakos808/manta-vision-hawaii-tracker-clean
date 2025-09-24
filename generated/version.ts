4eb88994b2a9a0a9c38eb724231f5665ccba020c
deakos808 <markdeakos@gmail.com>
2025-09-23 20:51:19 -1000

    checkpoint 20250923-205119 (hero + manta modal fixes)

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-09-24T06:27:20.459Z';
-export const GIT_HASH = '942c718';
+export const DEPLOYED_AT = '2025-09-24T06:40:37.502Z';
+export const GIT_HASH = '536bd1a';
