776feb12c9eaa5499c77134d412cc5cab6997904
deakos808 <markdeakos@gmail.com>
2025-09-27 01:30:33 -1000

    checkpoint 20250927-013033 (before maplibre harden)

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-09-27T11:26:30.467Z';
-export const GIT_HASH = '2f3c10c';
+export const DEPLOYED_AT = '2025-09-27T11:28:31.521Z';
+export const GIT_HASH = '4adc4ae';
