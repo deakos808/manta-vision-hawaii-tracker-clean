7b186989da2f8902d5ae9b63d7e2d4d0a562d389
deakos808 <markdeakos@gmail.com>
2025-09-27 01:37:21 -1000

    checkpoint 20250927-013721 (map picker overlay + drag)

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-09-27T11:30:48.750Z';
-export const GIT_HASH = '84ccb13';
+export const DEPLOYED_AT = '2025-09-27T11:33:35.443Z';
+export const GIT_HASH = '9fff3fc';
