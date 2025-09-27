1538d37b6e0dbe219ffb88abd115744b625954b5
deakos808 <markdeakos@gmail.com>
2025-09-27 07:55:30 -1000

    checkpoint 20250927-075530 (manual location overrides seed)

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-09-27T17:42:42.705Z';
-export const GIT_HASH = 'cd9dc15';
+export const DEPLOYED_AT = '2025-09-27T17:47:41.121Z';
+export const GIT_HASH = 'c746886';
