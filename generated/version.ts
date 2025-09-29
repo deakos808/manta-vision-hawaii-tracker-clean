c80e3ba2f9fa331eedbfdd923aa7aa3db171cfec
deakos808 <markdeakos@gmail.com>
2025-09-29 13:09:01 -1000

    UnifiedMantaModal: hard normalize React/supabase imports; remove stray useState; prevent shadowing

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-09-29T23:07:08.367Z';
-export const GIT_HASH = '2c03cf6';
+export const DEPLOYED_AT = '2025-09-29T23:09:01.230Z';
+export const GIT_HASH = '1be59e2';
