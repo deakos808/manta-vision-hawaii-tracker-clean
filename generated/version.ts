af4d9134e1879adee4227ea3c22eff7a19647b68
deakos808 <markdeakos@gmail.com>
2025-10-01 20:50:12 -1000

    AddSightingPage: always set pageMatchUrl from vBest before opening MatchModal

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-02T00:29:19.542Z';
-export const GIT_HASH = '85807d9';
+export const DEPLOYED_AT = '2025-10-02T06:08:00.062Z';
+export const GIT_HASH = 'd257652';
