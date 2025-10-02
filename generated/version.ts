4e7e8462c745a1df717e17be14194072408c15a8
deakos808 <markdeakos@gmail.com>
2025-10-01 21:15:01 -1000

    MatchModal: de-duplicate safeClose and leftSrc; insert single definitions; wire overlay/X to safeClose

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-02T07:00:38.339Z';
-export const GIT_HASH = '644e018';
+export const DEPLOYED_AT = '2025-10-02T07:10:13.094Z';
+export const GIT_HASH = 'ed166e6';
