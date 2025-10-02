0e916ea96214286d6ebb31ac778011f9b75cd839
deakos808 <markdeakos@gmail.com>
2025-10-01 21:20:07 -1000

    MatchModal: replace with single safeClose/leftSrc version and stable two-column UI

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-02T07:10:13.094Z';
-export const GIT_HASH = 'ed166e6';
+export const DEPLOYED_AT = '2025-10-02T07:15:17.210Z';
+export const GIT_HASH = '34a519a';
