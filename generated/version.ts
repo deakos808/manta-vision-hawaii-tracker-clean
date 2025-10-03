660cfb80e70324ec86c4690bc38bbf63dbeff21c
deakos808 <markdeakos@gmail.com>
2025-10-02 22:11:38 -1000

    AddSightingPage: fix 'e is not defined' by adding event params, correct paramless catch blocks, label pk_catalog_id, sweep stray backslashes

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-03T02:38:24.278Z';
-export const GIT_HASH = '177a437';
+export const DEPLOYED_AT = '2025-10-03T04:03:40.198Z';
+export const GIT_HASH = '0f0bd1e';
