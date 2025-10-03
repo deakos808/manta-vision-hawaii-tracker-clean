080b376407b089adcfa3a6dab04bf835c4021347
deakos808 <markdeakos@gmail.com>
2025-10-02 15:54:19 -1000

    AddSightingPage: fix ventral match JSX, per-row matched state, modal onChoose, remove stray backslashes

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-03T01:44:41.455Z';
-export const GIT_HASH = '41021df';
+export const DEPLOYED_AT = '2025-10-03T01:49:31.797Z';
+export const GIT_HASH = '453f415';
