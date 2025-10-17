a4a106c43d28fab56514f8fc80e582911c134999
deakos808 <markdeakos@gmail.com>
2025-10-17 07:59:42 -1000

    chore(admin): position Calibration card in visible section [2025-10-17T17-59-42-582Z]

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-17T16:37:36.124Z';
-export const GIT_HASH = '366b29f';
+export const DEPLOYED_AT = '2025-10-17T17:59:12.994Z';
+export const GIT_HASH = '65ebec9';
