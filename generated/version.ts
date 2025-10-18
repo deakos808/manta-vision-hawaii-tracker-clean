760db1fe089de7a20aa929ec505b28775d3aedfd
deakos808 <markdeakos@gmail.com>
2025-10-18 03:41:11 -1000

    feat(calibration): bottom blue Add photos pill; immediate upload (no measurements); filenames visible [2025-10-18T13-41-11-034Z]

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-18T07:39:25.699Z';
-export const GIT_HASH = 'b86db4e';
+export const DEPLOYED_AT = '2025-10-18T13:40:47.989Z';
+export const GIT_HASH = '9b2b5b5';
