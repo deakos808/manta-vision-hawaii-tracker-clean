baca92c9e19bb7edd6ce30309545bc2a4dbfe166
deakos808 <markdeakos@gmail.com>
2025-10-18 04:44:36 -1000

    style(admin): make Sighting Submissions a compact tile with icon; match dashboard card style [2025-10-18T14-44-36-655Z]

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-18T13:40:47.989Z';
-export const GIT_HASH = '9b2b5b5';
+export const DEPLOYED_AT = '2025-10-18T14:44:09.215Z';
+export const GIT_HASH = '760db1f';
