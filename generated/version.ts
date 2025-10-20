34bfcb7ab294ed4bbf0d4399b7432c4b03d1f82c
deakos808 <markdeakos@gmail.com>
2025-10-19 17:29:54 -1000

    hotfix(AddSightingPage): remove broken React.useMemo review fragments; add minimal useSearchParams flags

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-20T03:28:58.513Z';
-export const GIT_HASH = 'a298780';
+export const DEPLOYED_AT = '2025-10-20T03:29:08.840Z';
+export const GIT_HASH = '87ac00c';
