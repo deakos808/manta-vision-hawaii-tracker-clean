a2987803b2c5ac15c0b2a29840102227bbbe5f55
deakos808 <markdeakos@gmail.com>
2025-10-19 17:16:28 -1000

    chore(vite): enforce dev server on :8080 (strictPort, host)

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-20T03:15:21.542Z';
-export const GIT_HASH = '1b7173c';
+export const DEPLOYED_AT = '2025-10-20T03:16:07.768Z';
+export const GIT_HASH = 'c4ed692';
