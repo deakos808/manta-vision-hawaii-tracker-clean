86df239ff6ae29db1335f7c2708a2a17bf34d97c
deakos808 <markdeakos@gmail.com>
2025-10-20 05:37:53 -1000

    feat(review): JSX Save Changes button + handleSave wired to merge-safe saveReviewServer

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-20T08:25:09.821Z';
-export const GIT_HASH = 'ee6fe33';
+export const DEPLOYED_AT = '2025-10-20T15:37:33.071Z';
+export const GIT_HASH = '2601fca';
