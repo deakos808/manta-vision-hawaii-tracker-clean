25dc3095241a573718200b074c30277b388ff282
deakos808 <markdeakos@gmail.com>
2025-10-25 12:15:23 -1000

    restore: Mantas card grid + viewer + map multi-pin list from 98e6ec923d4c421f495809d748d255f3c05b7441

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-25T07:51:06.992Z';
-export const GIT_HASH = 'b20dcd3';
+export const DEPLOYED_AT = '2025-10-25T22:15:19.722Z';
+export const GIT_HASH = 'f7337a7';
