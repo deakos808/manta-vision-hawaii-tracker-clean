7db9d0b1321f8ea76b4ee99c3445fb9cb0c31d1c
deakos808 <markdeakos@gmail.com>
2025-10-25 20:46:59 -1000

    imports: add stg_catalog + helper views so groundtruth and dry-run work

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-26T06:37:11.650Z';
-export const GIT_HASH = '02fc027';
+export const DEPLOYED_AT = '2025-10-26T06:46:36.386Z';
+export const GIT_HASH = '9febfba';
