f099e914e01ccaf52b434b85a5e20c8658ddc9ce
deakos808 <markdeakos@gmail.com>
2025-10-25 20:11:02 -1000

    imports: stop mapper useEffect dependency loop; rely on typedMappings/ddl/computedSql only

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-26T04:22:56.502Z';
-export const GIT_HASH = 'fc70010';
+export const DEPLOYED_AT = '2025-10-26T05:43:04.139Z';
+export const GIT_HASH = '562ce7d';
