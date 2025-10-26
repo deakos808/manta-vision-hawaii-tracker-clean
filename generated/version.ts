7ba45f4519dc783d5bc41c7059c9c198071491f4
deakos808 <markdeakos@gmail.com>
2025-10-25 15:47:37 -1000

    imports: dynamic catalog commit with selectable columns + introspection view

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-26T01:25:44.695Z';
-export const GIT_HASH = '45e36f0';
+export const DEPLOYED_AT = '2025-10-26T01:26:10.604Z';
+export const GIT_HASH = '1bda6e4';
