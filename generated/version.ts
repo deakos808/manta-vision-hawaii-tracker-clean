31554c674f33057144d00ce47aa21bd303acd70f
deakos808 <markdeakos@gmail.com>
2025-10-25 20:11:52 -1000

    imports: add/repair groundtruth RPC and grants

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-26T05:43:04.139Z';
-export const GIT_HASH = '562ce7d';
+export const DEPLOYED_AT = '2025-10-26T06:11:03.073Z';
+export const GIT_HASH = 'f099e91';
