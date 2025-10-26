29254354f5e32c43d5c74fed6bc79acb94fc66ee
deakos808 <markdeakos@gmail.com>
2025-10-26 05:18:29 -1000

    imports: make Computed View Preview SQL paste-ready (comma before comments, no trailing comma)

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-26T14:53:51.366Z';
-export const GIT_HASH = '6148713';
+export const DEPLOYED_AT = '2025-10-26T14:57:38.831Z';
+export const GIT_HASH = '2ec075b';
