02fc027226faccd94a2183df895ff3fa4989a871
deakos808 <markdeakos@gmail.com>
2025-10-25 20:37:11 -1000

    imports: fix FieldMapper useEffect closing parenthesis

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2025-10-26T06:11:53.031Z';
-export const GIT_HASH = '31554c6';
+export const DEPLOYED_AT = '2025-10-26T06:28:23.387Z';
+export const GIT_HASH = 'daad956';
