d38f6ef93dbb27728baa0b4458028f6a8d84046a
deakos808 <markdeakos@gmail.com>
2026-03-04 23:12:36 -1000

    UI: restore HAMER logo in header

--- generated/version.ts
+++ generated/version.ts
@@ -1,2 +1,2 @@
-export const DEPLOYED_AT = '2026-03-05T09:05:26.021Z';
-export const GIT_HASH = '761f52bf';
+export const DEPLOYED_AT = '2026-03-05T09:12:30.153Z';
+export const GIT_HASH = '24a319a5';
